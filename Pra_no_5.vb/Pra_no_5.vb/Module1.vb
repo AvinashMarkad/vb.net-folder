15c0694354c36a17427e662623c9a0225ff7572a
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2022-07-21 16:02:11 +0530

    Add files via upload

---
+++ Pra_no_5.vb/Pra_no_5.vb/Module1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+
+        Dim c As Char
+        WriteLine("Enter any character to check vowel or not :")
+        c = ReadLine()
+
+        Select Case c
+
+            Case "a", "e", "i", "o", "u"
+                WriteLine("Character is vowel")
+
+            Case Else
+                WriteLine("Character is not vowel")
+
+        End Select
+
+        ReadLine()
+
+    End Sub
+
+End Module
---
+++ database5.java/database5.java/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Data.OleDb
+Module Module1
+
+    Dim con As OleDbConnection
+    Dim comd As OleDbCommand
+    Dim dr As OleDbDataReader
+
+    Sub Main()
+
+        con = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\marka\Documents\Database5.accdb")
+        con.Open()
+
+        comd = New OleDbCommand("select *from Table1", con)
+
+        dr = comd.ExecuteReader
+
+        While dr.Read()
+
+            Console.WriteLine("Roll no " & dr(0))
+            Console.WriteLine("Name " & dr(1))
+            Console.WriteLine("Branch " & dr(2))
+
+        End While
+        dr.Close()
+        con.Close()
+        Console.ReadLine()
+
+    End Sub
+
+End Module
