a2d23fd7feb1e20aa583803eff93d24bd3a5076b
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2022-07-21 16:10:05 +0530

    Add files via upload

---
+++ Pra_no_8.vb/Pra_no_8.vb/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+
+        a = TextBox1.Text
+
+        If a Mod 2 = 0 Then
+            Label2.Text = "Given number is Even number"
+
+        Else
+            Label2.Text = "Given number is odd number"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
