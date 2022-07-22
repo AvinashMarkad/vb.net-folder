b7e4ac78d15c72b5939d0319033b7f937f5eabeb
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2022-07-22 15:44:41 +0530

    Add files via upload

---
+++ Signal_Light.vb/Signal_Light.vb/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        ForeColor = Color.Red
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        ForeColor = Color.Yellow
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        ForeColor = Color.Green
+    End Sub
+End Class
