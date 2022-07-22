456394b37e5e16453b2a887f31aad6f68a3b0866
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2022-07-22 16:11:48 +0530

    Add files via upload

---
+++ test.vb/test.vb/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Class Shadow
+
+        Shared x As Integer = 1
+
+        Shared Shadows Sub Main()
+
+            Dim x As Integer = 10
+
+            Console.WriteLine("main: x" & x)
+
+            Console.WriteLine("main sahdow x:" & Shadow.x)
+
+            Console.ReadLine()
+        End Sub
+    End Class
+
+
+End Module
